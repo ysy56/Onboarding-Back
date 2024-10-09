8dd8029187fa7573953b2627bbf36daa47c99373
moony <78634780+ysy56@users.noreply.github.com>
2024-10-09 14:14:08 +0900

    update: nickname 추가

--- src/main/java/com/ysy56/onboardingback/dto/SignupRequestDto.java
+++ src/main/java/com/ysy56/onboardingback/dto/SignupRequestDto.java
@@ -12,4 +12,7 @@ public class SignupRequestDto {
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    private String nickname;
+
 }
