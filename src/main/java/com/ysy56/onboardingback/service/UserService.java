233f373c3b6758f7a56899bd8bca10ef8844fdc0
moony <78634780+ysy56@users.noreply.github.com>
2024-10-09 14:09:10 +0900

    update: 리포지토리 및 패스워드 인코더 추가

--- src/main/java/com/ysy56/onboardingback/service/UserService.java
+++ src/main/java/com/ysy56/onboardingback/service/UserService.java
@@ -1,10 +1,21 @@
 package com.ysy56.onboardingback.service;
 
+import com.ysy56.onboardingback.entity.UserAuthority;
+import com.ysy56.onboardingback.repository.AuthorityRepository;
+import com.ysy56.onboardingback.repository.UserAuthorityRepository;
+import com.ysy56.onboardingback.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final AuthorityRepository authorityRepository;
+    private final UserAuthorityRepository userAuthorityRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
 }
