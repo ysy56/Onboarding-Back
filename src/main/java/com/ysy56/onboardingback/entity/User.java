8beb70df560af8bc7ea8a76ab0f6d3f8a142e115
moony <78634780+ysy56@users.noreply.github.com>
2024-10-09 13:30:11 +0900

    fix: 변수명 철자 수정

--- src/main/java/com/ysy56/onboardingback/entity/User.java
+++ src/main/java/com/ysy56/onboardingback/entity/User.java
@@ -26,7 +26,7 @@ public class User {
     private String username;
 
     @Column(nullable = false)
-    private String passowrd;
+    private String password;
 
     @Column(nullable = false)
     private String nickname;
