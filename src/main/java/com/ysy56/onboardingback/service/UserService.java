71d4e7b2b86651670b5957b7f4d58f463b9cbccc
moony <78634780+ysy56@users.noreply.github.com>
2024-10-09 14:07:13 +0900

    add: UserService 생성

---
+++ src/main/java/com/ysy56/onboardingback/service/UserService.java
@@ -0,0 +1,10 @@
+package com.ysy56.onboardingback.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
