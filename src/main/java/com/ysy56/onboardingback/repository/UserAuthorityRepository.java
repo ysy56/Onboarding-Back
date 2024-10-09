a86be6b945b3394f500c474ac38bed341c2a470b
moony <78634780+ysy56@users.noreply.github.com>
2024-10-09 13:41:28 +0900

    add: Authority, User, UserAuthority 레파지토리 추가

---
+++ src/main/java/com/ysy56/onboardingback/repository/UserAuthorityRepository.java
@@ -0,0 +1,8 @@
+package com.ysy56.onboardingback.repository;
+
+import com.ysy56.onboardingback.entity.UserAuthority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAuthorityRepository extends JpaRepository<UserAuthority, Long> {
+
+}
