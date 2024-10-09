6aebe9d85b70e328f66f9060fd48408280bfb6fd
moony <78634780+ysy56@users.noreply.github.com>
2024-10-09 15:20:54 +0900

    update: 권한 존재여부 JPA 추가

--- src/main/java/com/ysy56/onboardingback/repository/AuthorityRepository.java
+++ src/main/java/com/ysy56/onboardingback/repository/AuthorityRepository.java
@@ -1,8 +1,11 @@
 package com.ysy56.onboardingback.repository;
 
 import com.ysy56.onboardingback.entity.Authority;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+
+    Optional<Authority> findByAuthorityName(String roleUser);
 
 }
